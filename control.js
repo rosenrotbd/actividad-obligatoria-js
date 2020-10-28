807d1c10c3800a71f70605690fa7ece6be2b4df6
rosenrotbd <58826880+rosenrotbd@users.noreply.github.com>
2020-10-28 17:00:31 -0300

    Add files via upload

---
+++ control.js
@@ -0,0 +1,18 @@
+const email=document.getElementById("email");
+email.addEventListener("input",function(even){
+if(email.validity.typeMismatch){
+	email.setCustomValidity("Falta el arroba en el usuario ")	;
+				}else{email.setCustomValidity(" ");}
+				});
+var form = document.querySelector('form');
+var fname = document.getElementById('email');
+var lname = document.getElementById('clave');
+var submit = document.getElementById('submit');
+var para = document.querySelector('p');
+	form.onsubmit = function(e) {
+ if (fname.value === '' || lname.value === '') {
+ e.preventDefault();
+ para.textContent = 'Completá ambos datos!';
+ }
+}
+
